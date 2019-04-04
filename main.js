e8c2fab6218df43f960590888a30a3f606d6a09b
kirubiel <39803957+kirubiel@users.noreply.github.com>
2019-04-04 09:13:54 +0300

    Final commit

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-document.getElementById('button').addEventListener('click', displayStuff);
+document.getElementById('new-quote').addEventListener('click', displayQuotes);
+document.body.onload = displayQuotes;
 
-function displayStuff() {
+let randomQuote = (input) => Math.floor(Math.random() * input);
+
+var text;
+var author;
+function displayQuotes() {
+
     fetch('quotes.json')
         .then((res) => res.json())
         .then((data) => {
-            document.getElementById('p').innerHTML = data.quotes[0].quote;
-            document.getElementById('anotherP').innerHTML = data.quotes[0].author;
-            document.getElementById('imgBackground').style.backgroundImage = `url(${data.quotes[0].image})`;
-        })
+            let randomIndex = randomQuote(data.quotes.length);
+            text = data.quotes[randomIndex].quote;
+            author = data.quotes[randomIndex].author;
+            let output = `
+            <img src="${data.quotes[randomIndex].image}">
+            <p id="text">${text}</p>
+            <p id="author">- ${author}</p>
+            `;
+            document.getElementById('quotes').innerHTML = output;
+        });
 }
+
+document.getElementById('tweet-quote').addEventListener('click', () => {
+    location.href='http://www.example.com';
+});
