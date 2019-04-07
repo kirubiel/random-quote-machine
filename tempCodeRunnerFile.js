47af337df8e1798fbfc9e53a613c7493af967cad
Kirubiel Shimeles <kirubiel123@gmail.com>
2019-04-07 16:35:51 +0300

    Removed jquery

--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('button');
-
-// button.addEventListener('click', () => {
-//     document.body.style.backgroundColor = "white";
-//     document.querySelector('p').textContent = "Hello";
-// });
